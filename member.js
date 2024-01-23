1044d511d0b2e49f0a180987f3b5e14f202f346d
kbordac <61948172+kbordac@users.noreply.github.com>
2024-01-23 19:30:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberValueLength = skillsMemberValue.length;
+    var skillsMemberValueLengthMax = skillsMember.getAttribute("maxlength");
+    var skillsMemberValueLengthMaxInt = parseInt(skillsMemberValueLengthMax);
+    var skillsMemberValueLengthMaxIntMinus = skillsMemberValueLengthMaxInt - skillsMemberValueLength;
+    var skillsMemberValueLengthMaxIntMinusString = skillsMemberValueLengthMaxIntMinus.toString();
+    skills.innerHTML = skillsMemberValueLengthMaxIntMinusString;
+    if (skillsMemberValueLengthMaxIntMinus <= 0) {
+        skills.style.color = "red";
+    } else {
+        skills.style.color = "black";
+    }
+}
